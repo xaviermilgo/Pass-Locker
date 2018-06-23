ea6b859fcf9de3ed9b57eef807031191f2ed7011
Xavier Kibet <xavierkibet@gmail.com>
2018-06-23 14:32:07 +0300

    add docstrings to credential object

--- creds.py
+++ creds.py
@@ -5,7 +5,13 @@ from os import urandom
 import pyperclip
 
 class Credential:
+	'''
+	Class that holds info about a credential
+	'''
 	def __init__(self,plaintext='',username='',userpass='',encrypted='',salt=''):
+		'''
+		Initializes both a new and existing credential object
+		'''
 		self.__password=plaintext
 		self.key=userpass
 		self.username=username
@@ -14,21 +20,36 @@ class Credential:
 		ekey=pbkdf2_hmac('sha256',self.key,self.salt, 100000)
 		self.enckey=urlsafe_b64encode(ekey)
 	def encrypt(self):
+		'''
+		Encrypts a credential object for storage using the mater key
+		'''
 		fernet=Fernet(self.enckey)
 		self.encrypted=fernet.encrypt(bytes(self.username+':::'+self.__password,'utf-8'))
 	def decrypt(self,hide=False):
+		'''
+		Decrypts a credential object in order to display it to the user
+		'''
 		fnet=Fernet(self.enckey)
 		uname,passw=fnet.decrypt(self.encrypted).decode('utf-8').split(':::')
 		if hide:
 			passw='*'*len(passw)
 		return [uname,passw]
 	def shiftkey(self,newpass):
+		'''
+		Rencrypts a credential after the user changes the master key
+		'''
 		old=Fernet(self.enckey)
 		ekey=pbkdf2_hmac('sha256',newpass,self.salt, 100000)
 		self.enckey=urlsafe_b64encode(ekey)
 		new=Fernet(self.enckey)
 		self.encrypted=new.encrypt(old.decrypt(self.encrypted))
 	def copy(self):
+		'''
+		copies the decrypted password to clipboard
+		'''
 		pyperclip.copy(self.decrypt()[1])
 	def export(self):
+		'''
+		Prepares the credential object for exportation
+		'''
 		return self.encrypted.decode('utf-8')+':'+urlsafe_b64encode(self.salt).decode('utf-8')
